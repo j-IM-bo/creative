43c195e993463c54b23e85eb24ed7e119a10f865
j-IM-bo <75038061+j-IM-bo@users.noreply.github.com>
2021-08-23 16:41:29 +0100

    set up

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//css
+import "./sass/style.scss";
+
+//js
+// import { gsap, Power1, Power2, Power3, Power4 } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// gsap.registerPlugin(ScrollTrigger);
+// import Flickity from "flickity";
+// import { debounce } from "./js/utilities/debounce";
+import { viewportSize } from "./js/utilities/viewportSize";
+
+function init() {
+    viewportSize();
+}
+
+window.addEventListener("load", function () {
+    init();
+});
